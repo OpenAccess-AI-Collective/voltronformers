81e18b9532654949bda9facbd05af4b9638ba713
Wing Lian <wing.lian@gmail.com>
2024-04-13 01:19:55 -0400

    flesh out rotary embeddigs

--- src/voltronformer/model.py
+++ src/voltronformer/model.py
@@ -13,6 +13,38 @@ from .core import Linear
 from .mod import MoDBlock
 
 
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    """Applies Rotary Position Embedding to the query and key tensors.
+
+    Args:
+        q (`torch.Tensor`): The query tensor.
+        k (`torch.Tensor`): The key tensor.
+        cos (`torch.Tensor`): The cosine part of the rotary embedding.
+        sin (`torch.Tensor`): The sine part of the rotary embedding.
+        unsqueeze_dim (`int`, *optional*, defaults to 1):
+            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+    Returns:
+        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+    """
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
+
+
 class FeedForward(nn.Module):
     def __init__(self, gate_proj: Linear, down_proj: Linear, up_proj: Linear):
         super().__init__()
@@ -65,6 +97,7 @@ class LlamaBitMGQA(BitMGQA):
     def forward(
             self,
             x: Tensor,
+            position_ids: Optional[Tensor] = None,
             need_weights: bool = False,
             # attn_mask: Optional[Tensor] = None,
             is_causal: bool = False,
@@ -79,6 +112,10 @@ class LlamaBitMGQA(BitMGQA):
         q = rearrange(q, "b n (h d) -> b n h d", h=self.query_heads)
         k = rearrange(k, "b n (h d) -> b n h d", h=self.kv_heads)
         v = rearrange(v, "b n (h d) -> b n h d", h=self.kv_heads)
+
+        # Apply rotary embedding.
+        q, k = apply_rotary_pos_emb(q, k, *self.rotary_emb(v, position_ids))
+
         # Apply attention, then fold 'h' attention heads back into 'd'.
         output, attn_weights = scaled_dot_product_gqa(
             query=q,
